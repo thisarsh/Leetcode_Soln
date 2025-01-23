762473b2de87cec2637d345250b02a0c3a180643
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-01-23 19:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.08%) - LeetSync

---
+++ Medium_Level/2556-convert-the-temperature/convert-the-temperature.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double c) {
+        double k,f;
+        k=c+273.15;
+        f=c*1.80+32.00;
+        vector <double> arr={k,f};
+        return arr;
+    }
+};
