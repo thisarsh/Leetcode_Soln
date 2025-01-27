8801fbba1a4bc5561890dc62a8353bd49e519d92
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-01-27 16:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (5.63%) - LeetSync

--- Solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ Solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+     //METHOD 1:
+     
+        set <int> st1(nums1.begin(),nums1.end());
+        set <int> st2(nums2.begin(),nums2.end());
+        
+        
+        nums1.clear();
+        
+        nums2.clear();
+    
+        nums1.insert(nums1.end(),st1.begin(),st1.end());
+        
+        nums1.insert(nums1.end(),st2.begin(),st2.end());
         sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        nums1.erase(unique(nums1.begin(),nums1.end()),nums1.end());
-        nums2.erase(unique(nums2.begin(),nums2.end()),nums2.end());
-              nums1.insert(nums1.end(),nums2.begin(),nums2.end());
-        unordered_map <int,int> mpp;
-        for(auto it: nums1){
-            mpp[it]++;
-        }        
-        vector <int> hash;
-        for (auto it: mpp){
-            if(it.second>1)hash.push_back(it.first);
+        for(int i=0; i<nums1.size()-1; i++){
+            if(nums1[i]==nums1[i+1])nums2.push_back(nums1[i]);
         }
-        return hash;
+
+     
+     return nums2;
+     
+     
+     
+     
+     //METHOD 2
+        // sort(nums1.begin(),nums1.end());
+        // sort(nums2.begin(),nums2.end());
+        // nums1.erase(unique(nums1.begin(),nums1.end()),nums1.end());
+        // nums2.erase(unique(nums2.begin(),nums2.end()),nums2.end());
+        //       nums1.insert(nums1.end(),nums2.begin(),nums2.end());
+        // unordered_map <int,int> mpp;
+        // for(auto it: nums1){
+        //     mpp[it]++;
+        // }        
+        // vector <int> hash;
+        // for (auto it: mpp){
+        //     if(it.second>1)hash.push_back(it.first);
+        // }
+        // return hash;
     }
 };
