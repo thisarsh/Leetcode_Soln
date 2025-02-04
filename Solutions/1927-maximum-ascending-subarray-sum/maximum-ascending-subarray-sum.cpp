7d733b3db9168ffb6afc4d42c3b42abc2c1160c6
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-02-04 12:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (97.52%) - LeetSync

--- Solutions/1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ Solutions/1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int n=nums.size(),gmax=0;
-        for(int i=0; i<n; i++){
-            int tempmax=0;
-            int prev=-1;
-            for(int j=i; j<n; j++){
-                if(nums[j]<=prev)break;
+        int n=nums.size(),gmax=nums[0],tempsum=nums[0];
+       for(int i=1; i<n; i++){
+            if(nums[i]>nums[i-1])tempsum+=nums[i];
+            else tempsum=nums[i];
+            gmax=max(gmax,tempsum);
 
-                tempmax+=nums[j];
-                gmax=max(gmax,tempmax);
-                prev=nums[j];
+       } return gmax;
+
+
+
+//             int prev=-1;
+//             for(int j=i; j<n; j++){
+//                 if(nums[j]<=prev)break;
+
+//                 tempmax+=nums[j];
+//                 gmax=max(gmax,tempmax);
+//                 prev=nums[j];
                 
-            }
-        }
-return gmax;
+//             }
+//         }
+// return gmax;
+// class Solution {
+// public:
+//     int maxAscendingSum(vector<int>& nums) {
+//         int gmax = nums[0], tempmax = nums[0];
 
-        
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (nums[i] > nums[i - 1]) { 
+//                 tempmax += nums[i];  // Continue the current increasing subarray
+//             } else {
+//                 tempmax = nums[i];  // Start a new subarray
+//             }
+//             gmax = max(gmax, tempmax);
+//         }
+
+//         return gmax;
     }
 };
+
+        
