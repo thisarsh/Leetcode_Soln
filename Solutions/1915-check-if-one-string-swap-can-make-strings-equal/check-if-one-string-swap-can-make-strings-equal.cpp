502a0649d19c83e90a6dbe8057304832393b71ae
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-02-05 23:32:39 +0530

    Time: 7 ms (5.04%) | Memory: 8.6 MB (19.06%) - LeetSync

--- Solutions/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ Solutions/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,6 +3,11 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         string s01=s1,s02=s2;
         if(s1==s2)return 1;
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        if(s1!=s2) return 0;
+        s1=s01;
+        s2=s02;        
         for(int i=0; i<s1.size()-1;i++){
             for(int j=1; j<s1.size();j++){
                 swap(s1[i],s1[j]);
