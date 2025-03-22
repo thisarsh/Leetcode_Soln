8570fde4cc40e5d7b1325434d1dc949f7b853f96
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-03-22 12:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (90.88%) - LeetSync

---
+++ Solutions/242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n1=s.size(),n2=t.size();
+        if(n1!=n2) return 0;
+        vector <int> hash(26,0);
+        for(int i=0; i<n1;i++){
+            hash[t[i]-'a']++;
+        }
+        vector <int> hash2(26,0);
+        for(int i=0; i<n1;i++){
+            hash2[s[i]-'a']++;
+        }
+        if(hash==hash2) return 1;
+        return 0;
+
+    }
+};
