ccc44314aae9163182250b9bbff07102fe8a8db2
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-03-25 21:55:52 +0530

    Time: 1 ms (52.26%) | Memory: 42.2 MB (16.78%) - LeetSync

---
+++ Solutions/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size(),p=0,lp=1,rp=1;
+        vector <int> lp_hash(n,1);
+        vector <int> rp_hash(n,1);
+        vector <int> ans_hash(n,1);
+        for(int i=0;i<n;i++){
+            lp_hash[i]*=lp;
+            lp*=nums[i];
+
+        }
+        for(int i=n-1;i>=0;i--){
+            rp_hash[i]*=rp;
+            rp*=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            ans_hash[i]=lp_hash[i]*rp_hash[i];
+        }
+        return ans_hash;
+            }
+};
