722906ae8da16f88a9347b0f7e33259e356c65a5
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-03-26 23:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (90.42%) - LeetSync

---
+++ Solutions/58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int l=0,n=s.size(),index=-1,ans=-1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!=' ')
+            {
+            index=i;
+            break;
+            }
+
+        }
+        // if (index=-1) return n;
+         for(int i=index;i>=0;i--){
+            if(s[i]!=' ')
+            l++;
+            else
+             break;
+            
+
+        }
+        return l;
+        
+    }
+};
