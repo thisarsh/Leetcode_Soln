9bef5b2a62fbd69f6b6fe384b5d809654ae8e151
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-04-01 22:49:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (93.97%) - LeetSync

--- Solutions/125-valid-palindrome/valid-palindrome.cpp
+++ Solutions/125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string a;
-        for (int i = 0; i < s.length(); i++) {
-            if ((s[i] >= 'a' and s[i] <= 'z') or
-                (s[i] <= 'Z' and s[i] >= 'A') or
-                (s[i] <= '9') and (s[i] >= '0'))
-
-                a += toupper(s[i]);
-        }
-        int n = a.length();
-        for (int i = 0; i < n; i++) {
-            if (a[i] != a[n - 1 - i])
-                return false;
-        }
-
-        return true;
+        int n=s.size(),i=0,j;
+        j=n-1;
+        while(i<=j){
+            if(!((s[i]>='0' && s[i]<='9')|| tolower(s[i])<='z' && (tolower(s[i])>='a'))) {i++;continue;}
+        if(!((s[j]>='0' && s[j]<='9')|| tolower(s[j])<='z' && (tolower(s[j])>='a'))) {j--;continue;}
+        if(tolower(s[i])!=tolower(s[j])) return 0;
+    i++;
+    j--;  
+    }return 1;
     }
 };
