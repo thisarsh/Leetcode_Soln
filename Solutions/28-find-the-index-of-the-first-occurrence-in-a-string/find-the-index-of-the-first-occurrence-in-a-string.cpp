d00b613e3e9056f60e1aa9abf8ca024016d848b3
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-06-21 14:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (57.81%) - LeetSync

--- Solutions/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ Solutions/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,18 +6,15 @@ public:
         else if(n2>n1)return -1;
         else if(n2==n1 && ne!=h)return -1;
 
-        // vector <string> hash((n1-n2+1),"");
+      
         string temp=h.substr(0,n2);
         for(int i=0; i<(n1-n2+1); i++){
-            // temp==ne;
             if(temp==ne)return i;
             temp=h.substr(i+1,n2);  
             
 
         }
-        // for(int i=0; i<hash.size();i++){
-        //     if(hash[i]==ne)return i;
-        // }
+    
         return -1;
     }
 };
