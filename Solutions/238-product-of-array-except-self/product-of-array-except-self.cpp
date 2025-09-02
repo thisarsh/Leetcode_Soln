9a83a2776ca0b9742cb8edd6172f5cc896dc9594
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-09-02 10:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.96%) - LeetSync

--- Solutions/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ Solutions/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size(),lp=1,rp=1;
-        vector <int> lp_hash(n,1);
-        vector <int> rp_hash(n,1);
-        vector <int> ans_hash(n,1);
-        for(int i=0;i<n;i++){
-            lp_hash[i]*=lp;
-            lp*=nums[i];
+        int n=nums.size(),tp=1,cp=1;
+      
+        // vector <int> ans(n,1);
+        vector <int> lp(n,1);
+        vector <int> rp(n,1);
+        
+        for(int i=0; i<n; i++){
+                lp[i]=tp;
+                tp*=nums[i];
+        }
 
+        for(int i=n-1; i>=0; i--)
+        {
+            rp[i]=cp;
+            cp*=nums[i];
         }
-        for(int i=n-1;i>=0;i--){
-            rp_hash[i]*=rp;
-            rp*=nums[i];
+        for(int i=0; i<n;i++){
+            nums[i]=lp[i]*rp[i];
         }
-        for(int i=0;i<n;i++){
-            ans_hash[i]=lp_hash[i]*rp_hash[i];
-        }
-        return ans_hash;
-            }
+
+        return nums;
+
+    }
 };
