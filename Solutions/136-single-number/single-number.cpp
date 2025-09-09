ec8cf44eb739c06cd05a117f4d3263883e9171e1
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-09-09 10:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (83.39%) - LeetSync

--- Solutions/136-single-number/single-number.cpp
+++ Solutions/136-single-number/single-number.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    //hashing by maps
-    int n=nums.size();
-    unordered_map <int,int> mpp;
-    for (auto it:nums){
-        mpp[it]++;
-    }    
-    for(auto it:mpp){
-        if (it.second!=2){
-            return it.first;
-        }
-    }  
-return 0;
+        int xorr=0;
+    for(auto it: nums){
+        xorr^=it;
+    }
+    return xorr;
+
+
+
+
+
+//     //hashing by maps
+//     int n=nums.size();
+//     unordered_map <int,int> mpp;
+//     for (auto it:nums){
+//         mpp[it]++;
+//     }    
+//     for(auto it:mpp){
+//         if (it.second!=2){
+//             return it.first;
+//         }
+//     }  
+// return 0;
     }
 };
