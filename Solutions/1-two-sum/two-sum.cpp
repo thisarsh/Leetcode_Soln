bda53ab1f00f7938641fc4ac0e238adf60cc4beb
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-09-10 12:22:47 +0530

    Time: 3 ms (70.19%) | Memory: 15.9 MB (6.09%) - LeetSync

--- Solutions/1-two-sum/two-sum.cpp
+++ Solutions/1-two-sum/two-sum.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector <int> hash(n,0);
 
-        for (int i=0; i<n; i++){
-            hash[i]=target-nums[i];
-   
+        int n=nums.size();
+        unordered_map <int,int> mpp;
+        for(int i=0; i<n; i++){
+            mpp[target-nums[i]]=i;
         }
-        for(int i=0 ; i<n; i++){
-          auto it=  find(hash.begin(),hash.end(),nums[i]);
-          if(it!=hash.end()){
-            int index=it-hash.begin();
-            if(index!=i)
-            return{index,i};
+        for(int i=0; i<n; i++){
+          
+          auto it=mpp.find(nums[i]);
+          if(it!=mpp.end() && it->second !=i)
+               { int index=it->second;
+                
+                return {i,index};
+            }
+        }
+
+
+
+
+
+
+
+
+        
+        // vector <int> hash(n,0);
+
+        // for (int i=0; i<n; i++){
+        //     hash[i]=target-nums[i];
+   
+        // }
+        // for(int i=0 ; i<n; i++){
+        //   auto it=  find(hash.begin(),hash.end(),nums[i]);
+        //   if(it!=hash.end()){
+        //     int index=it-hash.begin();
+        //     if(index!=i)
+        //     return{index,i};
             
-          }
-          }
+        //   }
+        //   }
         
         return {};
     }
