f16aa5f6c0a7bfc3bcd77a9f4dafaf4d0bc75feb
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-09-18 23:56:05 +0530

    Time: 1 ms (52.63%) | Memory: 9.9 MB (90.33%) - LeetSync

--- Solutions/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ Solutions/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,63 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size(),i;
-        string ans1="",temp="";
-
-
-       reverse(s.begin(),s.end());
-         for(i=0;i<n;i++){
-            
-            if(s[i]!=' '){
-               
-                temp+=s[i];
-   
-            }
-            else if (s[i]==' ' ){
-                reverse(temp.begin(),temp.end());
-                ans1+=temp;
-                if(temp!="")ans1+=" ";
-                temp="";
-            }
-            if((i==n-1)){
-                reverse(temp.begin(),temp.end());
-               ans1+= temp;
-            }
-         }
-            if(ans1[ans1.size()-1]==' '){
-                ans1.pop_back();
-
-            }
-
-         return ans1;
-
-
-
-
-        // int n=s.size(),i=0;
-        // vector <string> ans;
-        // string ok="";
-        // string temp="";
-        // for(int i=0; i<n; i++){
-        //     if(s[i]==' ')
-        //     {
-        //     if(temp.size()>=1)ans.push_back(temp);
-        //     temp="";
-        //     }
-          
-        //     else{
-        //         temp+=s[i];
-        //     }
-        //     if(i==n-1 && temp.size()>=1){
-        //         ans.push_back(temp);
-        //     }
-        // }
-        // for(int i=ans.size()-1;i>=0;i--){
-        //         ok+=ans[i];
-        //         if(i==0)return ok;
-        //         ok+=" ";
-        // }
-        // return ok;
-         
+        int n=s.size();
+        string ans="",fans="";
+        char prev=' ';
+        for(int i=n-1; i>=0; i--){
+           if(s[i]!=' '){
+            ans+=s[i];
+           }
+           else if (prev!=' '){
+              reverse(ans.begin(),ans.end());
+            fans+=(ans+" ");
+            ans="";
+           }
+           prev=s[i];
+        }
+        
+        reverse(ans.begin(),ans.end());
+        fans+=ans;
+        if(fans[fans.size()-1]==' ') fans.pop_back();
+        
+        return fans;
+        
     }
 };
