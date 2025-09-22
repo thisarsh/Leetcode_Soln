de3c784c95a773cb97d132df67d42db40fe8af7e
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-09-22 11:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.59%) - LeetSync

--- Solutions/172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ Solutions/172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,26 +1,11 @@
 class Solution {
+     int ans=0;
 public:
-
-    // int ans(int n){
-    //     if((n/5)<5 ){
-    //         return n/5;
-    //     }
-    // return n/5+ans(n/5);
-
-    // }
+   
     int trailingZeroes(int n) {
-        int ans=0;
-        while(n>=5){
-            ans+=n/5;
-            n/=5;
-        
+        if(n>5){
+            return (n/5)+trailingZeroes(n/5);
         }
-        
-        
-        
-            // if(n<5)return 0;
-            return ans;
-
-    
+        return n/5;
     }
 };
