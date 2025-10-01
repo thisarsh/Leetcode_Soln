673914fabe181630e787b80a12d0c89224915d15
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-10-02 02:37:54 +0530

    Time: 3 ms (81.89%) | Memory: 11.5 MB (82.97%) - LeetSync

--- Solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ Solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size(),l=0,r=0,ans=1;
-        if(n==0)return 0;
+        int n=s.size(),l=0,i=0,ans=0,temp=0;
         vector <int> hash(256,0);
- 
-        for(int i=0; i<n; i++){
-            hash[s[i]]++;
-
-            while(hash[s[i]]>1){
-                hash[s[l]]--;
-                l++;
-
-            }
-            ans=max(ans,i-l+1);
-
+       for(i=0; i<n;i++){
+         
+        
+        while(hash[s[i]]>0){
+            hash[s[l]]--;
+            l++;
 
         }
-        return ans;
+         hash[s[i]]++;
+     
+       
+
+        // hash[s[i]]++;
+        ans=max(ans,i-l+1);   
+
+
+       }
+           
+       return ans;
+        
     }
 };
