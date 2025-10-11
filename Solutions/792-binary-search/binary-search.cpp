9fef29ba7f6d4258a0f5b2d1e4256559ebafaa5d
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-10-11 19:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.17%) - LeetSync

--- Solutions/792-binary-search/binary-search.cpp
+++ Solutions/792-binary-search/binary-search.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int binary_s(vector<int>& nums,int low,int high,int target){
-        int mid=(high+low)/2;
-        if(nums[mid]==target)return mid;
-        else if(low>high)return -1;
-        else if(nums[mid]>target) return binary_s(nums,low,mid-1,target);
-        else return binary_s(nums,mid+1,high,target);
-    }
+
     int search(vector<int>& nums, int target) {
-        
         int n=nums.size();
-        int high=n-1;
-        int low=0;
-        // int mid=n/2;
-        return binary_s(nums,low,high,target);
-
-
-
+        int high=n-1,low=0;
+   
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target) {
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return -1;
 
 
     }
+    // int binary_s(vector<int>& nums,int low,int high,int target){
+    //     int mid=(high+low)/2;
+    //     if(nums[mid]==target)return mid;
+    //     else if(low>high)return -1;
+    //     else if(nums[mid]>target) return binary_s(nums,low,mid-1,target);
+    //     else return binary_s(nums,mid+1,high,target);
+    // }
+    // int search(vector<int>& nums, int target) {
+        
+    //     int n=nums.size();
+    //     int high=n-1;
+    //     int low=0;
+    //     // int mid=n/2;
+    //     return binary_s(nums,low,high,target);
+
+
+
+
+
+    // }
+
 };
