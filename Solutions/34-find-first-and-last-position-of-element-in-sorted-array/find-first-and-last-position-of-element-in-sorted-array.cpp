74a09a0c4662eddd9c638cdb96d8fe1263566e7e
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-10-13 02:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.33%) - LeetSync

--- Solutions/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ Solutions/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,49 @@
 class Solution {
 public:
-    int lower(vector <int> &nums,int target){
-        int n=nums.size();
-        int low=0,high=n-1,mid,poss=-1;
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                poss=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]>target){
-                high=mid-1;
-            }
-            else low=mid+1;
+    // int lower(vector <int> &nums,int target){
+    //     int n=nums.size();
+    //     int low=0,high=n-1,mid,poss=-1;
+    //     while(low<=high){
+    //         mid=low+(high-low)/2;
+    //         if(nums[mid]==target){
+    //             poss=mid;
+    //             high=mid-1;
+    //         }
+    //         else if(nums[mid]>target){
+    //             high=mid-1;
+    //         }
+    //         else low=mid+1;
 
-        }
-        return poss;
-    }
-    int upper(vector <int> &nums,int target){
-        int n=nums.size();
-        int low=0,high=n-1,mid,poss=-1;
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                poss=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]>target){
-                high=mid-1;
-            }
-            else low=mid+1;
+    //     }
+    //     return poss;
+    // }
+    // int upper(vector <int> &nums,int target){
+    //     int n=nums.size();
+    //     int low=0,high=n-1,mid,poss=-1;
+    //     while(low<=high){
+    //         mid=low+(high-low)/2;
+    //         if(nums[mid]==target){
+    //             poss=mid;
+    //             low=mid+1;
+    //         }
+    //         else if(nums[mid]>target){
+    //             high=mid-1;
+    //         }
+    //         else low=mid+1;
 
-        }
-        return poss;
-    }
+    //     }
+    //     return poss;
+    // }
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {lower(nums,target),upper(nums,target)};
+        // return {lower(nums,target),upper(nums,target)};
+        int x,y;
+        auto it=lower_bound(nums.begin(),nums.end(),target); 
+        x=it-nums.begin();
+        auto it2=upper_bound(nums.begin(),nums.end(),target); 
+        y=it2-nums.begin();
+        if(it==nums.end()|| *it!=target){
+            return {-1,-1};
+        }
+        return {x,y-1};
     }
 };
