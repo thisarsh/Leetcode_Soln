865c7aa51cf70fc5de29afb5416a047575f26fda
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-10-15 03:49:23 +0530

    Time: 8 ms (89.09%) | Memory: 40.2 MB (99.09%) - LeetSync

--- Solutions/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ Solutions/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -3,16 +3,17 @@ public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
       int count=1,i=0,j=0,ans=0,count2=0;
       int n=nums.size(),pl=0;
-      vector <int> hash;
+    //   vector <int> hash;
+    int prev=-1;
       if(k==1)return 1;
       for(int i=1; i<n; i++){
         if(nums[i-1]<nums[i]){
             count++;
-            if(count==k && hash.size()!=0 && (i-k)==hash.back())return 1;
+            if(count==k && prev!=-1 && (i-k)==prev)return 1;
             if(count==2*k)return 1;
         }
         else{
-           if(count>=k) hash.push_back(i-1);
+           if(count>=k) prev=(i-1);
            count=1;
 
         }
