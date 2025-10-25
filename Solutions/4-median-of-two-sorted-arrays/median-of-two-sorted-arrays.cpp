c9c0b00f2b51963322877cb3632cbd91f1da9d26
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-10-25 22:16:44 +0530

    Time: 1 ms (40.69%) | Memory: 96 MB (10.51%) - LeetSync

--- Solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ Solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,11 +4,27 @@ public:
       int m=nums1.size();
         int n=nums2.size();
         int p1=0,p2=0;
-        vector<int> neww(nums1.begin(),nums1.end());
-        for(auto it:nums2){
-            neww.push_back(it);
+        vector<int> neww;
+        while(p1<m && p2<n){
+            if(nums1[p1]>nums2[p2]){
+                neww.push_back(nums2[p2]);
+                p2++;
+            }
+            else{
+                neww.push_back(nums1[p1]);
+                p1++;
+
+            }
         }
-        sort(neww.begin(),neww.end());
+        while(p1<m){
+            neww.push_back(nums1[p1]);
+            p1++;
+        }       
+        while(p2<n){
+            neww.push_back(nums2[p2]);
+            p2++;
+        }       
+        // sort(neww.begin(),neww.end());
         if((m+n)%2==1){
         return float(neww[float(m+n)/2]);
         }
