3840074116d667b960a3e1504c1b14b926fb2107
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-11-12 11:11:05 +0530

    Time: 23 ms (52.31%) | Memory: 17.4 MB (83.12%) - LeetSync

--- Solutions/18-4sum/4sum.cpp
+++ Solutions/18-4sum/4sum.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        long long n = nums.size(), l, r, sum = 0;
+        int n=nums.size();
         if(n<4)return {};
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> answer;
-        for (int i = 0; i < n - 1; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            for (int j = i+1; j < n; j++) {
-                if (j > i+1 && nums[j] == nums[j - 1])
-                    continue;
-                l = j + 1;
-                r = n - 1;
-                while (l < r) {
-                    sum = (long long)nums[i]+nums[j]+nums[l]+nums[r];
-                    
-                    if (sum > target) {
-                        r--;
-                    } else if (sum < target) {
-                        l++;
-                    } else {
-                        answer.push_back({nums[i], nums[j], nums[l], nums[r]});
+     
+        sort(nums.begin(),nums.end());
+        vector <vector <int>> answer;
+        for(int i=0; i<n-3; i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1; j<n-2; j++){
+                if(j >i+1 && nums[j]==nums[j-1])continue;
+            long long sum=0;
+               int l=j+1,r=n-1;
+                while(l<r){
+                    sum=(long long)nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum==target){
+                        answer.push_back({nums[i],nums[j],nums[l],nums[r]});
                         l++;
                         r--;
-                        while (l < r && l > 0 && nums[l] == nums[l - 1]) {
-                            l++;
+                        while(l<r && l>0 && nums[l]==nums[l-1]){
+                                l++;
                         }
-                        while (l < r && r < n - 1 && nums[r] == nums[r + 1]) {
-                            r++;
+                        while(l<r && r<n-1 && nums[r]==nums[r+1]){
+                                r--;
                         }
                     }
+
+                        else if(sum<target){
+                            l++;
+                        }
+                        else r--;
+
+
+
+                    }
+
+
+
                 }
             }
-        }
+
+        
         return answer;
     }
 };
