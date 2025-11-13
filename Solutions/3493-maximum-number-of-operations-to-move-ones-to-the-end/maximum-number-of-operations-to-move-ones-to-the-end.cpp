1852c84fac21a71afa049a5c5786e0b89320bded
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-11-13 11:16:50 +0530

    Time: 12 ms (8.33%) | Memory: 15.9 MB (53.99%) - LeetSync

---
+++ Solutions/3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+       int l=0,r=0,n=s.size();
+       int oneseen=0,ans=0;
+      for(int i=0; i<n-1; i++){
+        if(s[i]=='1'){
+            oneseen++;
+        }
+        if(s[i]=='0' && s[i+1]=='1'){
+            ans+=oneseen;
+        }
+       
+        
+       }
+       if(s.back()=='0')ans+=oneseen;
+       return ans;
+    }
+};
