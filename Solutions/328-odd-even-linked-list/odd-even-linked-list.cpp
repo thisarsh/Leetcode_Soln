85ece9fb15aa3a2347b84516f310207403f318ee
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-11-16 23:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (27.30%) - LeetSync

--- Solutions/328-odd-even-linked-list/odd-even-linked-list.cpp
+++ Solutions/328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,27 +11,44 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        vector <int> ste;
-        vector <int> sto;
-        ListNode *temp=head;
-        int lene=0,len=0;
-        while(temp){
-            len++;
-            if(len%2==0)ste.push_back(temp->val);
-            else sto.push_back(temp->val);
-            temp=temp->next;
+        if(head==NULL || head->next==NULL)return head;
+        ListNode *odd=head;
+        ListNode *even=head->next;
+        ListNode *temp=even;
+        while(even && even->next ){
+            odd->next=odd->next->next;
+            odd=odd->next;
+            even->next=even->next->next;
+            even=even->next;
+
         }
-        temp=head;
-        for(auto it:sto){
-            temp->val=it;
-            temp=temp->next;
-            
-        }
-        for(auto it:ste){
-            temp->val=it;
-            temp=temp->next;
-            
-        }
+         
+        odd->next=temp;
+
+        
         return head;
+    
+        // vector <int> ste;
+        // vector <int> sto;
+        // ListNode *temp=head;
+        // int lene=0,len=0;
+        // while(temp){
+        //     len++;
+        //     if(len%2==0)ste.push_back(temp->val);
+        //     else sto.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // for(auto it:sto){
+        //     temp->val=it;
+        //     temp=temp->next;
+            
+        // }
+        // for(auto it:ste){
+        //     temp->val=it;
+        //     temp=temp->next;
+            
+        // }
+        // return head;
     }
 };
