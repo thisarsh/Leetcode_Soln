909e3d2355a58390f56b160c817b58a536e1fd47
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-11-17 21:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (74.62%) - LeetSync

--- Solutions/2-add-two-numbers/add-two-numbers.cpp
+++ Solutions/2-add-two-numbers/add-two-numbers.cpp
@@ -10,50 +10,79 @@
  */
 class Solution {
 public:
-    ListNode *newnode(ListNode *head,int data){
-        ListNode *nn=new ListNode(data,NULL);
-        if(head==NULL)return nn;
-        ListNode *temp=head;
-        while(temp->next){
-       temp=temp->next;
-        } temp->next=nn;
+    // ListNode *newnode(ListNode *head,int data){
+    //     ListNode *nn=new ListNode(data,NULL);
+    //     if(head==NULL)return nn;
+    //     ListNode *temp=head;
+    //     while(temp->next){
+    //    temp=temp->next;
+    //     } temp->next=nn;
 
-        return head;
-    }
+    //     return head;
+    // }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *ans= NULL;
-        ListNode *anshead= NULL;
-
-        ListNode *up=l1,*down=l2;
+        
+        ListNode *up=l1;
+        ListNode *down=l2;
+        ListNode *ans=new ListNode (0);
+        ListNode *anshead= ans;
         int carry=0;
-        while(up && down){
-            int curr=up->val+down->val+carry;
-            ans=newnode(ans,curr%10);
-            if(anshead==NULL){
-                anshead=ans;
+        while(up || down || carry>0){
+            int curr=carry;
+            if(up){
+             curr+=up->val;
+             up=up->next;
             }
+            if(down){
+                curr+=down->val;
+                down=down->next;
+
+            }
+            ans->next=new ListNode (curr%10);
+            ans=ans->next;
             carry=curr/10;
-            up=up->next;
-            down=down->next;
-        }
-        while(up){
-            int curr=up->val+carry;
-            ans=newnode(ans,curr%10);
-            carry=curr/10;
-            up=up->next;
-           
-        }
-        while(down){
-            int curr=down->val+carry;
-            ans=newnode(ans,curr%10);
-            carry=curr/10;
-            down=down->next;
-        }
-        if(carry>0){
-            ans=newnode(ans,carry);
+            
+            
         }
        
-       return anshead;
+
+return anshead->next;
+
+
+
+    //     ListNode *ans= NULL;
+    //     ListNode *anshead= NULL;
+
+    //     ListNode *up=l1,*down=l2;
+    //     int carry=0;
+    //     while(up && down){
+    //         int curr=up->val+down->val+carry;
+    //         ans=newnode(ans,curr%10);
+    //         if(anshead==NULL){
+    //             anshead=ans;
+    //         }
+    //         carry=curr/10;
+    //         up=up->next;
+    //         down=down->next;
+    //     }
+    //     while(up){
+    //         int curr=up->val+carry;
+    //         ans=newnode(ans,curr%10);
+    //         carry=curr/10;
+    //         up=up->next;
+           
+    //     }
+    //     while(down){
+    //         int curr=down->val+carry;
+    //         ans=newnode(ans,curr%10);
+    //         carry=curr/10;
+    //         down=down->next;
+    //     }
+    //     if(carry>0){
+    //         ans=newnode(ans,carry);
+    //     }
+       
+    //    return anshead;
         
     }
 };
