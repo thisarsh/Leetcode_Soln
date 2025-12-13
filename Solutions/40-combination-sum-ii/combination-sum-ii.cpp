5f02331ece1407b5bbef175525d4e695cdb06a5c
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-14 00:23:09 +0530

    Time: 2 ms (44.81%) | Memory: 14.2 MB (28.18%) - LeetSync

--- Solutions/40-combination-sum-ii/combination-sum-ii.cpp
+++ Solutions/40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-     void f(vector<int>& candidates, int target, vector <vector <int>>  & ans,
-     vector <int>&temp, int i, int sum ){
-        if(target==sum){
-            ans.push_back(temp);
-            return;
-        }
-        if(sum>target || i==candidates.size()){
+    void dfs( vector <int> &candidates,vector <vector <int>> &ans,
+        vector <int> &temp, int target, int i,int sum){
+        if(sum>=target ||i>=candidates.size()) {
+            if(sum==target)ans.push_back(temp);
             return;
         }
         temp.push_back(candidates[i]);
-        f(candidates,target,ans,temp,i+1,sum+candidates[i]);
-        temp.pop_back();
-        while(i+1<candidates.size() && candidates[i]==candidates[i+1]){
-            i++;
-        }
-       if(i<candidates.size()) f(candidates,target,ans,temp,i+1,sum);
+        dfs(candidates,ans,temp,target,i+1,sum+candidates[i]);
+        
+         temp.pop_back();
+        while(i<candidates.size()-1 && candidates[i]==candidates[i+1]) i++;
+       
+        dfs(candidates,ans,temp,target,i+1,sum);
         
 
-     }
+
+    }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector <vector <int>> ans;
         vector <int> temp;
         sort(candidates.begin(),candidates.end());
-        f(candidates,target,ans,temp,0,0);
+        dfs(candidates,ans,temp,target,0,0);
         return ans;
+
     }
 };
