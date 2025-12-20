599db3c51274f2072485d8e097f69f3341e22f1a
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-20 18:08:30 +0530

    Time: 3 ms (67.78%) | Memory: 14.7 MB (53.19%) - LeetSync

--- Solutions/1-two-sum/two-sum.cpp
+++ Solutions/1-two-sum/two-sum.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
         int n=nums.size();
         unordered_map <int,int> mpp;
         for(int i=0; i<n; i++){
-            mpp[target-nums[i]]=i;
-        }
-        for(int i=0; i<n; i++){
-          
-          auto it=mpp.find(nums[i]);
-          if(it!=mpp.end() && it->second !=i)
-               { int index=it->second;
-                
-                return {i,index};
+            auto it=mpp.find(nums[i]);
+            if(it!=mpp.end()){
+                return {i,it->second};
             }
+            mpp[target-nums[i]]=i;
+
         }
-
-
-
-
-
-
-
-
-        
-        // vector <int> hash(n,0);
-
-        // for (int i=0; i<n; i++){
-        //     hash[i]=target-nums[i];
-   
-        // }
-        // for(int i=0 ; i<n; i++){
-        //   auto it=  find(hash.begin(),hash.end(),nums[i]);
-        //   if(it!=hash.end()){
-        //     int index=it-hash.begin();
-        //     if(index!=i)
-        //     return{index,i};
-            
-        //   }
-        //   }
-        
         return {};
+        
     }
 };
