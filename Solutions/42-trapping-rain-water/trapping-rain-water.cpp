50b43a7d8a9c324dc64479b392fd4c22bf28ebaa
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-22 22:40:19 +0530

    Time: 2 ms (21.55%) | Memory: 26 MB (78.18%) - LeetSync

--- Solutions/42-trapping-rain-water/trapping-rain-water.cpp
+++ Solutions/42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        vector <int> pmax,smax;
-        int n=nums.size(),ans=0;
-        for(int i=0; i<n; i++){
-            if(pmax.size()==0 || pmax.back()<nums[i])pmax.push_back(nums[i]);
-            else pmax.push_back(pmax.back());
+        // vector <int> pmax,smax;
+        int n=nums.size(),ans=0,lmax=0,rmax=0;
+        int r=n-1,l=0;
+        while(l<r && r<n){
 
-        }
-        for(int i=n-1; i>=0; i--){
-            if(smax.size()==0 || smax.back()<nums[i])smax.push_back(nums[i]);
-            else smax.push_back(smax.back());
+            if(nums[l]<nums[r]){
+                lmax=max(lmax,nums[l]);
+                ans+=lmax-nums[l];
+                l++;
+            }
+            else{
+                 rmax=max(rmax,nums[r]);
+                 ans+=rmax-nums[r];
+                 r--;
+            }
+      
 
-        }
-        reverse(smax.begin(),smax.end());
-        for(int i=1; i<n-1; i++){
-            int sum=min(pmax[i],smax[i])-nums[i];
-            if(sum>0) ans+=sum;
         }
         return ans;
+
+
+
+
+
+
+
+
+
+        // for(int i=0; i<n; i++){
+        //     if(pmax.size()==0 || pmax.back()<nums[i])pmax.push_back(nums[i]);
+        //     else pmax.push_back(pmax.back());
+
+        // }
+        // for(int i=n-1; i>=0; i--){
+        //     if(smax.size()==0 || smax.back()<nums[i])smax.push_back(nums[i]);
+        //     else smax.push_back(smax.back());
+
+        // }
+        // reverse(smax.begin(),smax.end());
+        // for(int i=1; i<n-1; i++){
+        //     int sum=min(pmax[i],smax[i])-nums[i];
+        //     if(sum>0) ans+=sum;
+        // }
+        // return ans;
 
     }
 };
