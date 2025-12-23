578e0ee95f67af6fbd55dadeb3cfd9bac7eda54c
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-24 00:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (93.73%) - LeetSync

--- Solutions/22-generate-parentheses/generate-parentheses.cpp
+++ Solutions/22-generate-parentheses/generate-parentheses.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void gen(vector <string> &ans, int n,int open,int close,string s){
-        if(s.size()==n*2){
+    void f(vector <string> & ans, string &s, int open,int close, int n){
+        if(s.size()==2*n){
             ans.push_back(s);
             return;
         }
-      
-       if(open<n) {
-         
-          gen(ans,n,open+1,close,s+'(');
-       }
-        if(close<open){
-             
-             gen(ans,n,open,close+1,s+')');
+        if( open <n){
+            s+='(';
+            f(ans,s,open+1,close,n);
+            s.pop_back();
+
         }
-
-
-
+        if(close<open){
+            s+=')';
+            f(ans,s,open,close+1,n);
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector <string> ans;
-        string s="";
-        gen(ans,n,0,0,s);
+        string s;
+        f(ans,s,0,0,n);
         return ans;
-
     }
 };
