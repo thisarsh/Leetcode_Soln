ad4e66f9a96176be46154d5cec55dc75230d8d23
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-12-24 22:58:56 +0530

    Time: 2 ms (56.08%) | Memory: 55 MB (60.25%) - LeetSync

--- Solutions/41-first-missing-positive/first-missing-positive.cpp
+++ Solutions/41-first-missing-positive/first-missing-positive.cpp
@@ -1,13 +1,60 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+       
 
-    int n=nums.size(),ans;
-    unordered_set <int> st(nums.begin(),nums.end());
-    int a=1;
-        while(st.find(a)!=st.end()){
-             a++;
-        }return a;
+        for(auto &it: nums){
+            if (it > n || it <=0){
+                it=n+1;
+            }
+        }
+
+
+    for(int i=0; i<n; i++){ 
+        
+        int el=abs(nums[i]);
+        if(el==n+1) continue;      
+        if(nums[el-1]>0) nums[el-1]*=-1;
+    }
+
+       for(int i=0; i<n; i++){ 
+            if(nums[i]>0) return i+1;
+       }
+       return n+1;
+
+
+
+
+
+
+
+      
+        
+
+
+    // int n=nums.size(),ans,xora=0,min=INT_MAX,max=INT_MIN;
+    // for(int i=0; i<n; i++){
+    //     if(nums[i]<min && nums[i]>0){
+    //         min=nums[i];
+    //     }
+    //     if(nums[i]>max && nums[i]>0){
+    //         max=nums[i];
+    //     }
+    //     if(nums[i]>0){
+    //         xora^=nums[i];
+    //     }
+    // }
+    
+    // for(int i=min; i<=max; i++){
+    //     xora^=i;
+    // }
+    // if(min>1){
+    //     return 1;
+    // }
+    // if(xora==0)return max+1;
+    
+    // return xora;
 
     }
 };
