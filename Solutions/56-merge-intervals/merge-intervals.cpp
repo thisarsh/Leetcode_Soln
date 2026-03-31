891eb95c4fd958dbbf204257fd04611c2c4a8d3d
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-04-01 02:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (80.92%) - LeetSync

--- Solutions/56-merge-intervals/merge-intervals.cpp
+++ Solutions/56-merge-intervals/merge-intervals.cpp
@@ -7,9 +7,9 @@ public:
         sort(intervals.begin(),intervals.end());
         ans.push_back(intervals.front());
         for(int i=1; i<n; i++){
-              if(intervals[i][1]<ans.back()[1] )continue;
-           else  if(intervals[i][0]<=ans.back()[1])
-            ans.back()[1]=intervals[i][1];
+              
+           if(intervals[i][0]<=ans.back()[1])
+            ans.back()[1]=max(intervals[i][1],ans.back()[1]);
           
 
             else ans.push_back({intervals[i][0],intervals[i][1]});
