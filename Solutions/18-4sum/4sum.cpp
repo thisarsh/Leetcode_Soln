85d56bee877e502b138b9073933ba3a1db05a7b5
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-03-31 21:03:40 +0530

    Time: 19 ms (80.50%) | Memory: 17.3 MB (98.53%) - LeetSync

--- Solutions/18-4sum/4sum.cpp
+++ Solutions/18-4sum/4sum.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        if(n<4)return {};
-     
+        long long int n=nums.size(),sum=0;
         sort(nums.begin(),nums.end());
-        vector <vector <int>> answer;
+        vector <vector <int>> ans;
         for(int i=0; i<n-3; i++){
             if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1; j<n-2; j++){
-                if(j >i+1 && nums[j]==nums[j-1])continue;
-            long long sum=0;
-               int l=j+1,r=n-1;
-                while(l<r){
-                    sum=(long long)nums[i]+nums[j]+nums[l]+nums[r];
+                if( j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1,l=n-1;
+                while(k<l){
+                    sum=(long long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target){
-                        answer.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        l++;
-                        r--;
-                        while(l<r && l>0 && nums[l]==nums[l-1]){
-                                l++;
-                        }
-                        while(l<r && r<n-1 && nums[r]==nums[r+1]){
-                                r--;
-                        }
-                    }
-
-                        else if(sum<target){
-                            l++;
-                        }
-                        else r--;
-
-
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && l<n-1 && nums[l+1]==nums[l]) l--;
+                        
 
                     }
-
-
-
+                    else if(sum>target) l--;
+                    else k++;
                 }
             }
-
-        
-        return answer;
+        }
+        return ans;
     }
 };
