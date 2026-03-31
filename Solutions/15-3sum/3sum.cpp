966c83394d89cdae16874a67b5cc8b99c949956b
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-03-31 15:44:25 +0530

    Time: 47 ms (66.76%) | Memory: 29.1 MB (45.54%) - LeetSync

--- Solutions/15-3sum/3sum.cpp
+++ Solutions/15-3sum/3sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size(), tsum = 0, l = 1, r, ans = 0, i = 0;
-        r = n - 1;
-        vector<vector<int>> answer;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
-            if ( i>0 && nums[i] == nums[i - 1])
-                continue;
-            l = i + 1;
-            r = n - 1;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        vector <vector <int>> ans;
+        for(int i=0; i<n-2; i++){
+            if(i> 0 && nums[i-1]==nums[i])continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                   while(j<k && nums[j]==nums[j-1]) j++;
+                    while(k>j && nums[k]==nums[k+1])k--;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else j++;
 
-            while (l < r ) {
-
-                tsum = nums[i] + nums[l] + nums[r];
-                if (tsum > 0) {
-                    r--;
-                } else if (tsum < 0) {
-                    l++;
-                } else {
-                    answer.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
                 
-                while (l < r && l>0 && nums[l] == nums[l - 1] ) {
-                    l++;
-                }
-                while (l < r && r<n-1 && nums[r] == nums[r + 1]) {
-                    r--;
-                }
-            }
             }
         }
-        // set<vector<int>> st(answer.begin(), answer.end());
-        // answer.assign(st.begin(), st.end());
-        return answer;
+        return ans;
+        
+
+
+        
     }
 };
