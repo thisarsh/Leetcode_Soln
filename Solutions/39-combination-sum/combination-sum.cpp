995a8cb1e4acb582e2677daa8d9016d22cbddd4d
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-02 21:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (91.96%) - LeetSync

--- Solutions/39-combination-sum/combination-sum.cpp
+++ Solutions/39-combination-sum/combination-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void f(  vector <vector <int>> &ans,vector<int>& candidates, 
-    int target,int i, vector <int> &nums,int sum){
+    void f(vector <int> &candidates, int target, int i, int sum,  vector <vector  <int>> &ans,vector <int> &curr){
+
         if(sum==target){
-            ans.push_back(nums);
-            return ;
+            ans.push_back(curr);
+            return;
         }
-        if(sum>target || i==candidates.size())return;
-        
-            nums.push_back(candidates[i]);
-            f(ans,candidates,target,i,nums,sum+candidates[i]);
-
-    
-            nums.pop_back();
-            // nums.push_back(candidates[i]);
-            f(ans,candidates,target,i+1,nums,sum);
-
+        if(sum>target){
+            return;
+        }
+        if(i==candidates.size()) return;
+        curr.push_back(candidates[i]);
+        f(candidates,target,i,sum+candidates[i],ans,curr);
+        sum-=curr.back();
+        curr.pop_back();
+        f(candidates,target,i+1,sum+candidates[i],ans,curr);
         
 
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector <vector <int>> ans;
-        vector <int> nums;
-        sort(candidates.begin(),candidates.end());
-        f(ans,candidates,target,0,nums,0);
+        vector <int> curr;
+        f(candidates,target,0,0,ans,curr);
         return ans;
+        
     }
 };
