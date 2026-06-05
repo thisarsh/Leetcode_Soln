c21c47dbb3d2d45bafcd2ac792f5670ccaacab89
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-06 03:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (46.58%) - LeetSync

--- Solutions/100-same-tree/same-tree.cpp
+++ Solutions/100-same-tree/same-tree.cpp
@@ -10,17 +10,15 @@
  * };
  */
 class Solution {
-    bool  rec(TreeNode *p,TreeNode *q){
-        if(p==NULL && q==NULL) return 1;
-        if(p==NULL  || q==NULL) return 0;
-        if(p->val!=q->val) return 0;
-         return rec( p->left,q->left) && rec(p->right , q->right); 
-            
-        
-
-    }
 public:
+    bool dfs(TreeNode *p, TreeNode *q){
+        if(!p && !q) return 1;
+        if((!p ||!q)) return 0;
+        if(dfs(p->left,q->left) && dfs(p->right, q->right) && (p->val==q->val))return 1;
+return 0;
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return rec(p,q);
+        return dfs(p,q);
+        
     }
 };
